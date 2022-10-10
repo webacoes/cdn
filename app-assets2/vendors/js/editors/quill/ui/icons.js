9976d3c5283e22ebe4c2d6c03c50136db466bcd3
webacoes <webacoes@gmail.com>
2022-10-10 18:48:03 -0300

    upload
    Ok

---
+++ app-assets2/vendors/js/editors/quill/ui/icons.js
@@ -0,0 +1,48 @@
+module.exports = {
+  'align': {
+    ''        : require('../assets2/icons/align-left.svg'),
+    'center'  : require('../assets2/icons/align-center.svg'),
+    'right'   : require('../assets2/icons/align-right.svg'),
+    'justify' : require('../assets2/icons/align-justify.svg')
+  },
+  'background': require('../assets2/icons/background.svg'),
+  'blockquote': require('../assets2/icons/blockquote.svg'),
+  'bold'      : require('../assets2/icons/bold.svg'),
+  'clean'     : require('../assets2/icons/clean.svg'),
+  'code'      : require('../assets2/icons/code.svg'),
+  'code-block': require('../assets2/icons/code.svg'),
+  'color'     : require('../assets2/icons/color.svg'),
+  'direction' : {
+    ''        : require('../assets2/icons/direction-ltr.svg'),
+    'rtl'     : require('../assets2/icons/direction-rtl.svg')
+  },
+  'float': {
+    'center'  : require('../assets2/icons/float-center.svg'),
+    'full'    : require('../assets2/icons/float-full.svg'),
+    'left'    : require('../assets2/icons/float-left.svg'),
+    'right'   : require('../assets2/icons/float-right.svg')
+  },
+  'formula'   : require('../assets2/icons/formula.svg'),
+  'header': {
+    '1'       : require('../assets2/icons/header.svg'),
+    '2'       : require('../assets2/icons/header-2.svg')
+  },
+  'italic'    : require('../assets2/icons/italic.svg'),
+  'image'     : require('../assets2/icons/image.svg'),
+  'indent': {
+    '+1'      : require('../assets2/icons/indent.svg'),
+    '-1'      : require('../assets2/icons/outdent.svg')
+  },
+  'link'      : require('../assets2/icons/link.svg'),
+  'list': {
+    'ordered' : require('../assets2/icons/list-ordered.svg'),
+    'bullet'  : require('../assets2/icons/list-bullet.svg')
+  },
+  'script': {
+    'sub'     : require('../assets2/icons/subscript.svg'),
+    'super'   : require('../assets2/icons/superscript.svg'),
+  },
+  'strike'    : require('../assets2/icons/strike.svg'),
+  'underline' : require('../assets2/icons/underline.svg'),
+  'video'     : require('../assets2/icons/video.svg')
+};
